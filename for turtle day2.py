a6ff2615393f00184ba4feb241d574558e37e481
WorachartPOP <103193501+WorachartPOP@users.noreply.github.com>
2022-04-07 19:48:40 +0700

    การบ้าน day 2 ดูย้อนหลังครับ

---
+++ for turtle day2.py
@@ -0,0 +1,11 @@
+import turtle #import function turtle
+tao = turtle.Pen()
+def pop():
+    for i in range(100):
+        tao.forward(200)
+        tao.left(45)
+        tao.forward(20)
+        tao.circle(60)
+        tao.left(36)
+
+    
